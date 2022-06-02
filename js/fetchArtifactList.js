bb3dd9ec0db8ab333a75db7ff55eda58828f9d7b
Raoul Hofmann <raouldhofmann@gmail.com>
2022-06-02 09:23:35 +0800

    Update fetchArtifactList.js

--- js/fetchArtifactList.js
+++ js/fetchArtifactList.js
@@ -9,7 +9,7 @@ function get() {
     headers.append('Accept', 'application/json');
     headers.append('Authorization', 'Bearer 78e17825b6b7d0edd54a98dce33ba49aaaed8bdb0e22636b24b8894afccf42d9f1d8197810fd0ffa9b439ee7e862d646b984efb7c568236b6d259069078d1bbb57ec020ddacec9cd6245656fa9ad7669f2a1be7efedc1b0820fa35ce7ed32cf6452bfb73dfb6366f6159e4ed60663fb22c941ca76e80937222d2c11ad8516714');
     
-    fetch('http://artifact-travelogues.668558.xyz/api/artifacts', {
+    fetch('https://artifact-travelogues.668558.xyz/api/artifacts', {
         method: 'GET',
         headers: headers
     })
